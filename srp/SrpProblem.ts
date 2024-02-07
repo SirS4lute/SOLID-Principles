2002e140050d4107769661e4309acce0c1f43532
sirs4lute <william-salute@hotmail.com>
2024-02-07 19:07:21 -0300

    Fix the word "responsibility"

--- srp/SrpProblem.ts
+++ srp/SrpProblem.ts
@@ -24,14 +24,12 @@ export class Character {
 
 /*
  (US) SRP Explained: 
-    The class must have one unique responsability, single responsability principle!
-    We'll see a lot of aggregated objects and dependency injection for this to work
+    The class must have one unique responsibility, single responsibility principle!
     
     Here we have a problem, the method create job doesn't belongs to character, but to job (line: 8). So why are we coding the createJob method in our Character class? See the SrpSolution.
 
  (BR) SRP Explicado: 
     A classe deve ter uma responsabilidade única, princípio de responsabilidade única!
-    Veremos muitos objetos agregados e injeção de dependência para isso funcionar
     
     Aqui temos um problema, o método create job não pertence ao personagem, mas sim ao job (linha: 8). Então, por que estamos codificando o método createJob em nossa classe Character? Consulte a solução Srp.
 */
